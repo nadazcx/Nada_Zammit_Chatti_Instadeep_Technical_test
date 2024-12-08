3b9c75e6f47558206ca2f2f657c7ffb0e07ed2b0
Nada <zcnadaa@gmail.com>
2024-12-08 11:09:16 +0100

    adding comments

--- programming_challenge/palindrome.py
+++ programming_challenge/palindrome.py
@@ -1,8 +1,16 @@
-
 def palindrome(description):
-    description_alpha=''
+    description_alpha = ''
+    # This loop removes non alphabets from the string and lower the chars
     for i in range(len(description)):
         if description[i].lower().isalpha():
-            description_alpha+=description[i].lower()
-    print(description_alpha)
-    return description_alpha==description_alpha[::-1]
+            description_alpha += description[i].lower()
+
+    # we comapares the string by reversing it using [::-1]
+    return description_alpha == description_alpha[::-1]
+
+
+## TESTING INPUTS
+input_true = "Madam, in Eden, I'm Adam"
+print(palindrome(input_true))
+input_false = "Complete the project report"
+print(palindrome(input_false))
